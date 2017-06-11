35c42a21e34f20bb74154fa6e238a4daeb0d96d9
wonwoo <aoruqjfu@gmail.com>
2017-06-11 20:13:33 +0900

    error 추가

--- src/main/java/me/wonwoo/CollectionsFactory.java
+++ src/main/java/me/wonwoo/CollectionsFactory.java
@@ -16,8 +16,12 @@ public class CollectionsFactory {
     Set<Integer> setNumbers = Set.of(1, 2, 3, 4, 5);
     System.out.println(setNumbers);
 
+//    Set.of(1, 1, 3, 4, 5); //error
+
     Map<String, String> map = Map.of("key1", "value1", "key2", "value2");
     System.out.println(map);
+
+//    Map.of("key1", "value1", "key1", "value2"); //error
 
 //    listNumbers.add(6); //error
 //    setNumbers.add(6); //error
